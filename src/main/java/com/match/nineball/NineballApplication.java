e031e0a8215db6fc3b5d06a0643afd2c8ae5c526
blancgoat <blancgoat@gmail.com>
2022-03-28 21:51:37 +0900

    first commit

---
+++ src/main/java/com/match/nineball/NineballApplication.java
@@ -0,0 +1,13 @@
+package com.match.nineball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NineballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NineballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/match/nineball/NineballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.match.nineball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NineballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
